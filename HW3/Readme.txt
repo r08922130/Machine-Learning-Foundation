940f30a781fd6ba8c45fbd1e7dbde358677a1078
丁杰 <dingjie@dingjies-MacBook-Pro.local>
2019-12-25 14:48:09 +0800

    chart

---
+++ HW3/Readme.txt
@@ -0,0 +1,11 @@
+GD 
+SGD
+
+Question 7
+
+Use 0/1 Error as E_in
+Question 8
+
+Use 0/1 Error as E_out
+
+
--- HW3/logistic.py
+++ HW3/logistic.py
@@ -1,7 +1,7 @@
 import math
 import numpy as np
 from random import *
-import  matplotlib.pyplot as plt
+import matplotlib.pyplot as plt
 def data_load(path):
 
     arr = np.loadtxt(path)
@@ -35,51 +35,62 @@ def err0_1(w,x,y):
 #x0 = 1
 x = np.hstack((np.ones(x.shape[0]).reshape(-1,1),x))
 step = 2000
-lr = [0.01,0.001]
+lrs = [0.01,0.001]
 
 
 is_SGD = [False, True]
 (test_x,test_y) = data_load("hw3_test.dat.txt")
 test_x = np.hstack((np.ones(test_x.shape[0]).reshape(-1,1),test_x))
-q_7 = plt.figure("Question 7")
-q_8 = plt.figure("Question 8")
+
 sign =[ 'k--' ,'k:']
 method = ['GD','SGD']
 E_in_history = []
 E_out_history = []
 steps =[]
-for j in range(len(is_SGD)):
-    #w0 = 0
-    w = np.zeros(x.shape[1])
-    
-    for i in range(0,step):
+for lr in lrs:
+    for j in range(len(is_SGD)):
+        #w0 = 0
+        w = np.zeros(x.shape[1])
+        
+        for i in range(0,step):
 
-        if is_SGD[j]:
-            ## Q 20
-            x_n = np.array(x[i%x.shape[0]])
-            y_n = np.array(y[i%y.shape[0]])
-        else :
-            ## Q 19
-            x_n = x
-            y_n = y
-        steps += [i+1]
-        E_in_history += [err0_1(w,x,y)]
-        gradient = cal_gradient(w,x_n,y_n)
-        w = update_W(w,lr[j],gradient)
-        E_out_history +=[err0_1(w,test_x,test_y)]
-    
-    E_out = err0_1(w,test_x,test_y)
-    print("=====E_out ( 0/1 err )=====")
-    print(E_out)
-plt.figure("Question 7")
-plots = plt.plot(steps[:step],E_in_history[:step],sign[0],steps[:step],E_in_history[step:],sign[1])
+            if is_SGD[j]:
+                ## Q 20
+                x_n = np.array(x[i%x.shape[0]])
+                y_n = np.array(y[i%y.shape[0]])
+            else :
+                ## Q 19
+                x_n = x
+                y_n = y
+            steps += [i+1]
+            E_in_history += [err0_1(w,x,y)]
+            gradient = cal_gradient(w,x_n,y_n)
+            w = update_W(w,lr,gradient)
+            E_out_history +=[err0_1(w,test_x,test_y)]
+        
+        E_out = err0_1(w,test_x,test_y)
+        print("=====E_out ( 0/1 err )=====")
+        print(E_out)
+
+plt.figure("lr=0.01_Question 7")
+plots = plt.plot(steps[:step],E_in_history[:step],sign[0],steps[:step],E_in_history[step:2*step],sign[1])
 plt.legend(plots ,(method[0],method[1]))
-plt.savefig("Question_7")
-plt.figure("Question 8")
-plots = plt.plot(steps[:step],E_out_history[:step],sign[0],steps[:step],E_out_history[step:],sign[1])
+plt.savefig("lr=0.01_Question_7.png")
+plt.figure("lr=0.01_Question 8")
+plots = plt.plot(steps[:step],E_out_history[:step],sign[0],steps[:step],E_out_history[step:2*step],sign[1])
 plt.legend(plots ,(method[0],method[1]))
-plt.savefig("Question_8")
+plt.savefig("lr=0.01_Question_8.png")
+plt.figure("lr=0.001_Question 7")
+plots = plt.plot(steps[:step],E_in_history[2*step:3*step],sign[0],steps[:step],E_in_history[3*step:],sign[1])
+plt.legend(plots ,(method[0],method[1]))
+plt.savefig("lr=0.001_Question_7.png")
+plt.figure("lr=0.001_Question 8")
+plots = plt.plot(steps[:step],E_out_history[2*step:3*step],sign[0],steps[:step],E_out_history[3*step:],sign[1])
+plt.legend(plots ,(method[0],method[1]))
+plt.savefig("lr=0.001_Question_8.png")
+
 plt.show()
+
 
 
 
